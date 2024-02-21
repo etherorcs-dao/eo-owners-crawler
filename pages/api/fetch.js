6b5b64af348f9be585f94819c143bdbbcc65c758
0xTempest <tempest@etherdrakes.com>
2024-02-20 18:39:08 -0600

    change

--- pages/api/fetch.js
+++ pages/api/fetch.js
@@ -1,21 +1,17 @@
-import {getFarmingConfigs, getFarmingSessions} from "@/utils.js";
+import {fetch, init} from "@/core.js";
 
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
 
 export default async function handler(req, res) {
-  /*init().then(() => {
-    fetch().then((res1) => {
-      console.log(res1);
-      res1.status(200).json({ success: true });
-    }).catch((err) => {
-      console.error(err);
-      res.status(500).json({ success: false });
+    init().then(() => {
+        fetch().then((res1) => {
+            console.log(res1);
+            res1.status(200).json({success: true});
+        }).catch((err) => {
+            console.error(err);
+            res.status(500).json({success: false});
+        });
+    }).catch(() => {
+        res.status(500).json({success: false});
     });
-  }).catch(() => {
-    res.status(500).json({ success: false });
-  });*/
-
-
-  await getFarmingSessions();
-  await getFarmingConfigs();
 }
